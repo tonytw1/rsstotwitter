7cffb6152e01856e4264a80fa1f906a5701ddba6
Tony McCrae <tony@eelpieconsulting.co.uk>
2014-04-28 22:21:14 +1200

    Clean up - pulling out method.

--- src/main/java/nz/gen/wellington/rsstotwitter/feeds/FeedDAO.java
+++ src/main/java/nz/gen/wellington/rsstotwitter/feeds/FeedDAO.java
@@ -1,7 +1,6 @@
 package nz.gen.wellington.rsstotwitter.feeds;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -10,6 +9,7 @@ import nz.gen.wellington.rsstotwitter.model.FeedItem;
 
 import org.apache.log4j.Logger;
 
+import com.google.common.collect.Lists;
 import com.sun.syndication.feed.module.georss.GeoRSSModule;
 import com.sun.syndication.feed.module.georss.GeoRSSUtils;
 import com.sun.syndication.feed.synd.SyndEntry;
@@ -17,33 +17,22 @@ import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.fetcher.FeedFetcher;
 import com.sun.syndication.fetcher.impl.HttpURLFeedFetcher;
 
-public class FeedDAO {
+public class FeedDAO {	// TODO not really a database DAO - rename
 
-    public final Logger log = Logger.getLogger(FeedDAO.class);
+    public final static Logger log = Logger.getLogger(FeedDAO.class);
 
     @SuppressWarnings("unchecked")
 	public List<FeedItem> loadFeedItems(Feed feed) {
-    	List<FeedItem> feedItems = new ArrayList<FeedItem>();
+    	List<FeedItem> feedItems = Lists.newArrayList();
     	SyndFeed syndfeed = loadSyndFeedWithFeedFetcher(feed.getUrl());
     	if (syndfeed == null) {
     		log.warn("Could not load syndfeed from url: " + feed.getUrl() + ". Returning empty list of items");
     		return null;
     	}
     	
-        Iterator<SyndEntry> feedItemsIterator = syndfeed.getEntries().iterator();
+        final Iterator<SyndEntry> feedItemsIterator = syndfeed.getEntries().iterator();
         while (feedItemsIterator.hasNext()) {        	
-        	SyndEntry feedItem = (SyndEntry) feedItemsIterator.next();
-        	
-        	Double latitude = null;
-        	Double longitude = null;        	
-			GeoRSSModule geoModule = (GeoRSSModule) GeoRSSUtils.getGeoRSS(feedItem);
-			if (geoModule != null && geoModule.getPosition() != null) {
-				latitude = geoModule.getPosition().getLatitude();
-				longitude = geoModule.getPosition().getLongitude();
-				log.debug("Rss item '" + feedItem.getTitle() + "' has position information: " + latitude + "," + longitude);
-			}
-        	
-        	feedItems.add(new FeedItem(feed, feedItem.getTitle(), feedItem.getUri(), feedItem.getLink(), feedItem.getPublishedDate(), feedItem.getAuthor(), latitude, longitude));
+        	feedItems.add(mapFeedItem(feed, (SyndEntry) feedItemsIterator.next()));
         }
         return feedItems;
     }
@@ -62,5 +51,17 @@ public class FeedDAO {
         }
         return null;
     }
-
+    
+    private FeedItem mapFeedItem(Feed feed, SyndEntry syndEntry) {
+		Double latitude = null;
+		Double longitude = null;
+		GeoRSSModule geoModule = (GeoRSSModule) GeoRSSUtils.getGeoRSS(syndEntry);
+		if (geoModule != null && geoModule.getPosition() != null) {
+			latitude = geoModule.getPosition().getLatitude();
+			longitude = geoModule.getPosition().getLongitude();
+			log.debug("Rss item '" + syndEntry.getTitle() + "' has position information: " + latitude + "," + longitude);
+		}		
+		return new FeedItem(feed, syndEntry.getTitle(), syndEntry.getUri(), syndEntry.getLink(), syndEntry.getPublishedDate(), syndEntry.getAuthor(), latitude, longitude);
+	}
+    
 }
