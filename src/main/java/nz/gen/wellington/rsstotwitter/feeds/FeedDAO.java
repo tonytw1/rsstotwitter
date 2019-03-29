2ca5d06936b31a1b1f9bbf9f7665ca0e803c9cb3
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-03-29 22:43:12 +0000

    Clean ups

--- src/main/java/nz/gen/wellington/rsstotwitter/feeds/FeedDAO.java
+++ src/main/java/nz/gen/wellington/rsstotwitter/feeds/FeedDAO.java
@@ -1,14 +1,5 @@
 package nz.gen.wellington.rsstotwitter.feeds;
 
-import java.net.URL;
-import java.util.Iterator;
-import java.util.List;
-
-import nz.gen.wellington.rsstotwitter.model.Feed;
-import nz.gen.wellington.rsstotwitter.model.FeedItem;
-
-import org.apache.log4j.Logger;
-
 import com.google.common.collect.Lists;
 import com.sun.syndication.feed.module.georss.GeoRSSModule;
 import com.sun.syndication.feed.module.georss.GeoRSSUtils;
@@ -16,7 +7,14 @@ import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.fetcher.FeedFetcher;
 import com.sun.syndication.fetcher.impl.HttpURLFeedFetcher;
+import nz.gen.wellington.rsstotwitter.model.Feed;
+import nz.gen.wellington.rsstotwitter.model.FeedItem;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
 
 @Component
 public class FeedDAO {	// TODO not really a database DAO - rename
@@ -42,12 +40,12 @@ public class FeedDAO {	// TODO not really a database DAO - rename
     private SyndFeed loadSyndFeedWithFeedFetcher(String feedUrl) {
         log.info("Loading SyndFeed from url: " + feedUrl);
     
-        URL url;
+        ;
         try {
-            url = new URL(feedUrl);
-            FeedFetcher fetcher = new HttpURLFeedFetcher();            
-            SyndFeed feed = fetcher.retrieveFeed(url);
-            return feed;
+            URL url = new URL(feedUrl);
+            FeedFetcher fetcher = new HttpURLFeedFetcher();
+            return fetcher.retrieveFeed(url);
+
         } catch (Exception e) {
             log.warn("Error while fetching feed: " + e.getMessage());
         }
