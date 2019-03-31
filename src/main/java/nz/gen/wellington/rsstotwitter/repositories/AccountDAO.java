3af860280f55f8e679b4b06c393155fa2d43f3e9
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-03-31 12:18:22 +0100

    Refactor; extract interface for accounts persistances... ahead of migrating to different storage.

--- src/main/java/nz/gen/wellington/rsstotwitter/repositories/AccountDAO.java
+++ src/main/java/nz/gen/wellington/rsstotwitter/repositories/AccountDAO.java
@@ -1,41 +1,13 @@
 package nz.gen.wellington.rsstotwitter.repositories;
 
-import java.util.List;
-
 import nz.gen.wellington.rsstotwitter.model.TwitterAccount;
 
-import org.hibernate.criterion.DetachedCriteria;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.hibernate4.HibernateTemplate;
-import org.springframework.stereotype.Component;
+import java.util.List;
 
-@Component
-public class AccountDAO {
-	
-    private final HibernateTemplate hibernateTemplate;
+public interface AccountDAO {
+    void saveAccount(TwitterAccount account);
 
-    @Autowired
-	public AccountDAO(HibernateTemplate hibernateTemplate) {	
-		this.hibernateTemplate = hibernateTemplate;
-	}
-	
-	public void saveAccount(TwitterAccount account) {	
-		hibernateTemplate.saveOrUpdate(account);
-	}
+    TwitterAccount getUserByTwitterId(long id);
 
-	@SuppressWarnings("unchecked")
-	public TwitterAccount getUserByTwitterId(long id) {
-		List<TwitterAccount> accounts = (List<TwitterAccount>) hibernateTemplate.findByCriteria(DetachedCriteria.forClass(TwitterAccount.class).add(Restrictions.eq("id", id)));
-        if (!accounts.isEmpty()) {
-        	return accounts.get(0);
-        }
-        return null;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<TwitterAccount> getAllTwitterAccounts() {
-		return hibernateTemplate.loadAll(TwitterAccount.class);
-	}
-
+    List<TwitterAccount> getAllTwitterAccounts();
 }
---
+++ src/main/java/nz/gen/wellington/rsstotwitter/repositories/MysqlAccountDAO.java
@@ -0,0 +1,44 @@
+package nz.gen.wellington.rsstotwitter.repositories;
+
+import java.util.List;
+
+import nz.gen.wellington.rsstotwitter.model.TwitterAccount;
+
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate4.HibernateTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MysqlAccountDAO implements AccountDAO {
+	
+    private final HibernateTemplate hibernateTemplate;
+
+    @Autowired
+	public MysqlAccountDAO(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
+	}
+	
+	@Override
+	public void saveAccount(TwitterAccount account) {
+		hibernateTemplate.saveOrUpdate(account);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public TwitterAccount getUserByTwitterId(long id) {
+		List<TwitterAccount> accounts = (List<TwitterAccount>) hibernateTemplate.findByCriteria(DetachedCriteria.forClass(TwitterAccount.class).add(Restrictions.eq("id", id)));
+        if (!accounts.isEmpty()) {
+        	return accounts.get(0);
+        }
+        return null;
+	}
+	
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<TwitterAccount> getAllTwitterAccounts() {
+		return hibernateTemplate.loadAll(TwitterAccount.class);
+	}
+
+}
