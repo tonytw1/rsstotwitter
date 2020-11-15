1328492591be273b2a5a50948e80976239e8f0a4
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-11-15 11:28:26 +0000

    Hello world dao test.

---
+++ src/test/java/nz/gen/wellington/rsstotwitter/repositories/mongo/MongoFeedToTwitterJobDAOTest.java
@@ -0,0 +1,28 @@
+package nz.gen.wellington.rsstotwitter.repositories.mongo;
+
+import nz.gen.wellington.rsstotwitter.model.FeedToTwitterJob;
+import org.junit.Test;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class MongoFeedToTwitterJobDAOTest {
+
+    String mongoDatabase = "rsstotwittertest" + UUID.randomUUID().toString();
+
+    DataStoreFactory dataStoreFactory = new DataStoreFactory("localhost:27017", mongoDatabase, "", "", false);
+    MongoFeedToTwitterJobDAO dao = new MongoFeedToTwitterJobDAO(dataStoreFactory);
+
+    @Test
+    public void canSaveJobAndReloadById() {
+        FeedToTwitterJob job = new FeedToTwitterJob();
+
+        dao.save(job);
+        assertNotNull(job.getObjectId());
+
+        FeedToTwitterJob reloaded = dao.getByObjectId(job.getObjectId());
+        assertEquals(job.getObjectId(), reloaded.getObjectId());
+    }
+
+}
