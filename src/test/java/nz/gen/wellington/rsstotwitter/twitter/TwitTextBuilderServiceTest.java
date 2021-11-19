8f3497b2629b2ea821e44016c8ec1ed4e69229b5
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-11-19 11:20:29 +0000

    Introduce Twitter's twittertext library and use to assert some things about or generated tweets.

--- src/test/java/nz/gen/wellington/rsstotwitter/twitter/TwitTextBuilderServiceTest.java
+++ src/test/java/nz/gen/wellington/rsstotwitter/twitter/TwitTextBuilderServiceTest.java
@@ -1,27 +1,31 @@
 package nz.gen.wellington.rsstotwitter.twitter;
 
-import static junit.framework.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
+import com.twitter.twittertext.Extractor;
+import com.twitter.twittertext.TwitterTextParseResults;
+import com.twitter.twittertext.TwitterTextParser;
 import nz.gen.wellington.rsstotwitter.model.Feed;
 import nz.gen.wellington.rsstotwitter.model.FeedItem;
 import nz.gen.wellington.rsstotwitter.tinyurl.TinyUrlService;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class TwitTextBuilderServiceTest {
 
     private static final String PUBLISHER_NAME = "A Publisher";
     private static final String TITLE = "The quick brown fox";
-    private static final String LONG_URL = "http://www.longurl/etc";
+    private static final String LONG_URL = "http://www.longurl.com/etc/1234567890/abcdefghijk";
+    private static final String SHORT_URL = "http://tinyurl.com/12345";
 
     @Mock
     TinyUrlService tinyUrlService;
@@ -33,7 +37,7 @@ public class TwitTextBuilderServiceTest {
     public void setUp() throws IOException {
         MockitoAnnotations.initMocks(this);
         tinyUrlService = mock(TinyUrlService.class);
-        when(tinyUrlService.makeTinyUrl(LONG_URL)).thenReturn("http://tinyurl/1");
+        when(tinyUrlService.makeTinyUrl(LONG_URL)).thenReturn(SHORT_URL);
         service = new TwitTextBuilderService(tinyUrlService);
     }
 
@@ -46,8 +50,18 @@ public class TwitTextBuilderServiceTest {
     @Test
     public void shouldConvertLinksIntoTinyUrls() throws IOException {
         final String twit = service.buildTwitForItem(new FeedItem(feed, TITLE, LONG_URL, LONG_URL, null, null, Optional.empty()));
-        verify(tinyUrlService).makeTinyUrl(LONG_URL);
-        assertEquals("The quick brown fox http://tinyurl/1", twit);
+
+        assertEquals("The quick brown fox http://tinyurl.com/12345", twit);
+
+        TwitterTextParseResults twitterTextParseResults = TwitterTextParser.parseTweet(twit);
+        assertTrue(twitterTextParseResults.isValid);
+        // Twitter seems to let you have the space for free
+        assertEquals(TITLE.length() + SHORT_URL.length(), twitterTextParseResults.weightedLength);
+
+        final Extractor extractor = new Extractor();
+        List<String> urls = extractor.extractURLs(twit);
+        assertEquals(1, urls.size());
+        assertEquals("http://tinyurl.com/12345",urls.get(0));
     }
 
     @Test
