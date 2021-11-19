81b6df4d0124fb438a067b61023e8d92d1db25aa
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-11-19 11:31:12 +0000

    Remove tinyurl shorterning because twitter will apply a t.co link regardless.

--- src/main/java/nz/gen/wellington/rsstotwitter/twitter/TwitTextBuilderService.java
+++ src/main/java/nz/gen/wellington/rsstotwitter/twitter/TwitTextBuilderService.java
@@ -1,31 +1,22 @@
 package nz.gen.wellington.rsstotwitter.twitter;
 
 import nz.gen.wellington.rsstotwitter.model.FeedItem;
-import nz.gen.wellington.rsstotwitter.tinyurl.TinyUrlService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Component
 public class TwitTextBuilderService {
 
     private static final String DASH_SEPARATOR = " - ";
 
-    private final TinyUrlService tinyUrlService;
-
-    @Autowired
-    public TwitTextBuilderService(TinyUrlService tinyUrlService) {
-        this.tinyUrlService = tinyUrlService;
+    public TwitTextBuilderService() {
     }
 
-    public String buildTwitForItem(FeedItem feedItem) throws IOException {
+    public String buildTwitForItem(FeedItem feedItem) {
         StringBuffer twit = new StringBuffer();
         twit.append(feedItem.getTitle());
         if (feedItem.getLink() != null) {
-            final String tinyUrlLink = tinyUrlService.makeTinyUrl(feedItem.getLink());
             twit.append(" ");
-            twit.append(tinyUrlLink);
+            twit.append(feedItem.getLink());
         }
 
         if (feedItem.getAuthor() != null && !feedItem.getAuthor().isEmpty()) {
@@ -38,7 +29,7 @@ public class TwitTextBuilderService {
         final String publisherPrefix = publisher + DASH_SEPARATOR;
         final boolean publisherWillFit = (twit.length() + publisherPrefix.length()) <= TwitterSettings.MAXIMUM_TWITTER_MESSAGE_LENGTH;
         if (publisherWillFit) {
-            return publisherPrefix + twit.toString();
+            return publisherPrefix + twit;
         } else {
             return twit.toString();
         }
--- src/test/java/nz/gen/wellington/rsstotwitter/twitter/TwitTextBuilderServiceTest.java
+++ src/test/java/nz/gen/wellington/rsstotwitter/twitter/TwitTextBuilderServiceTest.java
@@ -5,11 +5,7 @@ import com.twitter.twittertext.TwitterTextParseResults;
 import com.twitter.twittertext.TwitterTextParser;
 import nz.gen.wellington.rsstotwitter.model.Feed;
 import nz.gen.wellington.rsstotwitter.model.FeedItem;
-import nz.gen.wellington.rsstotwitter.tinyurl.TinyUrlService;
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import java.io.IOException;
 import java.util.List;
@@ -17,29 +13,16 @@ import java.util.Optional;
 
 import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class TwitTextBuilderServiceTest {
 
     private static final String PUBLISHER_NAME = "A Publisher";
     private static final String TITLE = "The quick brown fox";
     private static final String LONG_URL = "http://www.longurl.com/etc/1234567890/abcdefghijk";
-    private static final String SHORT_URL = "http://tinyurl.com/12345";
 
-    @Mock
-    TinyUrlService tinyUrlService;
-    TwitTextBuilderService service;
+    TwitTextBuilderService service = new TwitTextBuilderService();
 
-    Feed feed;
-
-    @Before
-    public void setUp() throws IOException {
-        MockitoAnnotations.initMocks(this);
-        tinyUrlService = mock(TinyUrlService.class);
-        when(tinyUrlService.makeTinyUrl(LONG_URL)).thenReturn(SHORT_URL);
-        service = new TwitTextBuilderService(tinyUrlService);
-    }
+    private final Feed feed = new Feed();
 
     @Test
     public void shouldIgnoreLinkIfSetToNull() throws IOException {
@@ -48,32 +31,33 @@ public class TwitTextBuilderServiceTest {
     }
 
     @Test
-    public void shouldConvertLinksIntoTinyUrls() throws IOException {
+    public void shouldKnowThatUrlsAreShorterned() throws IOException {
         final String twit = service.buildTwitForItem(new FeedItem(feed, TITLE, LONG_URL, LONG_URL, null, null, Optional.empty()));
 
-        assertEquals("The quick brown fox http://tinyurl.com/12345", twit);
+        assertEquals("The quick brown fox http://www.longurl.com/etc/1234567890/abcdefghijk", twit);
 
         TwitterTextParseResults twitterTextParseResults = TwitterTextParser.parseTweet(twit);
         assertTrue(twitterTextParseResults.isValid);
-        // Twitter seems to let you have the space for free
-        assertEquals(TITLE.length() + SHORT_URL.length(), twitterTextParseResults.weightedLength);
+        // Twitter applies t.co shorter to all urls so all urls count as length 23
+        assertTrue(LONG_URL.length() > 23);
+        assertEquals(TITLE.length() + " ".length() + 23, twitterTextParseResults.weightedLength);
 
         final Extractor extractor = new Extractor();
         List<String> urls = extractor.extractURLs(twit);
         assertEquals(1, urls.size());
-        assertEquals("http://tinyurl.com/12345",urls.get(0));
+        assertEquals("http://www.longurl.com/etc/1234567890/abcdefghijk",urls.get(0));
     }
 
     @Test
     public void shouldNotAppendChannelIfNotSet() throws IOException {
         final String twit = service.buildTwitForItem(new FeedItem(feed, TITLE, LONG_URL, LONG_URL, null, null, Optional.empty()));
-        assertEquals("The quick brown fox http://tinyurl/1", twit);
+        assertEquals("The quick brown fox http://www.longurl.com/etc/1234567890/abcdefghijk", twit);
     }
 
     @Test
     public void shouldPrependPublisher() throws IOException {
         final String twit = service.buildTwitForItem(new FeedItem(feed, TITLE, LONG_URL, LONG_URL, null, PUBLISHER_NAME, Optional.empty()));
-        assertEquals("A Publisher - The quick brown fox http://tinyurl/1", twit);
+        assertEquals("A Publisher - The quick brown fox http://www.longurl.com/etc/1234567890/abcdefghijk", twit);
     }
 
 }
