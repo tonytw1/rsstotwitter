da701fc6b688af24e7db16332aecc3adaab05383
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-11-24 21:11:06 +0000

    Fix incorrect counts; filter by destination.

--- src/main/java/nz/gen/wellington/rsstotwitter/repositories/mongo/TwitterHistoryDAO.java
+++ src/main/java/nz/gen/wellington/rsstotwitter/repositories/mongo/TwitterHistoryDAO.java
@@ -71,6 +71,7 @@ public class TwitterHistoryDAO {
                 greaterThan(since).
                 filter("feed.url", feed.getUrl()).
                 filter("account", account).
+                filter("destination", destination).
                 count();
     }
 
